3743e8769d702bbb26533b9b779c29befe7d5acd
zc_lee <lizhuochao@forchange.tech>
2026-01-09 11:15:31 +0800

    feat: 🎸 修改学生组织zod验证规则

--- src/domain/schemas/studentOrganizations.ts
+++ src/domain/schemas/studentOrganizations.ts
@@ -3,8 +3,7 @@ import { z } from "zod";
 export const studentOrganizationsSchema = z.object({
   id: z.string().min(1),
   name: z.string().min(1),
-  pid: z.string().optional(),
-  year: z.number().int(),
+  pid: z.string().nullable().optional(), // 允许数据库返回 null
+  year: z.preprocess((val) => Number(val), z.number().int()), // 自动把字符串 "0" 转成数字 0
   code: z.string().optional(),
 });
-
